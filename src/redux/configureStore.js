cf71097b078d8315684d06389cb5d8e48cae9283
JinZhang <tripzhang05@gmail.com>
2021-08-01 19:24:09 -0400

    Intro to Redux

---
+++ src/redux/configureStore.js
@@ -0,0 +1,10 @@
+import { createStore} from 'redux';
+import { reducer, initialState} from './reducer';
+
+export const ConfigureStore = () =>{
+    const store = createStore(
+        reducer,
+        initialState
+    );
+    return store;
+};
