32b0bd540930ceeb25234b65ae7cfc974ced7f9e
JinZhang <tripzhang05@gmail.com>
2021-08-08 15:55:47 -0400

    Redux thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import {
     Button, Modal, ModalHeader, ModalBody
 } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 
 function RenderCampsite({ campsite }) {
@@ -45,6 +46,29 @@ function RenderComments({ comments, addComment, campsiteId }) {
 }
 
 function CampsiteInfo(props) {
+
+    if (props.isLoading) {
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <div className='col'>
+                        <h4>{props.errMess}</h4>
+                    </div>
+                    
+                </div>
+            </div>
+        );
+    }
+
     if (props.campsite) {
         return (
             <div className='container'>
